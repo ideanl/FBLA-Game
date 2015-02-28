8fdc65d36b2da464d62e5a8937af92fc0083e17d
Idean <idean1.labib@gmail.com>
2015-02-28 11:02:43 -0800

    Added Portal script

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//PORTAL REQUIRES: RIGIDBODY(NO GRAVITY) AND COLLIDER(IS TRIGGER = TRUE)
+public class Portal : MonoBehaviour {
+
+	public Transform matchingPortal;
+
+	//Upon instantiation, find the matching portal.
+	void Awake() {
+		foreach (Transform child in transform.parent) {
+			if (child.gameObject.GetInstanceID () != this.gameObject.GetInstanceID ()) {
+				matchingPortal = child;
+			}
+		}
+	}
+
+	// When something intersects the portal.
+	void OnTriggerEnter (Collider collider) {
+		//If it's a player make it pop out of the matching portal.
+		if (collider.gameObject.tag == "Player") {
+			Transform player = collider.gameObject.transform;
+			player.position = matchingPortal.transform.position;
+			player.rotation = matchingPortal.transform.rotation;
+			Bounds bounds = transform.gameObject.renderer.bounds;
+			float width = Vector3.Project(bounds.max - bounds.min, player.forward).magnitude;
+			player.position += player.forward * width;
+			//TODO: play an audio sound
+		}
+	}
+}
