e8a36a2680b27c21cc4dc5bc8755020f6eb09a35
Idean <idean1.labib@gmail.com>
2015-03-01 23:00:34 -0800

    fixed merge issues

--- Assets/Scripts/Click/Pickup.cs
+++ Assets/Scripts/Click/Pickup.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class Pickup : Click {
 
-<<<<<<< HEAD
-	public Transform player;
-
-	void Awake() {
-		player = GameObject.FindGameObjectsWithTag ("Player")[0].transform;
-	}
-
-	protected override void ClickAction() {
-		if (Vector3.Distance (player.position, transform.position) < 2) {
-=======
 	public GameObject player;
 
 	void Awake() {
@@ -21,17 +11,9 @@ public class Pickup : Click {
 
 	public override void ClickAction() {
 		if (Vector3.Distance (player.transform.position, transform.position) < 2) {
->>>>>>> origin/master
 			player.GetComponent<CustomCharacter> ().items.Add(transform.gameObject);
 			transform.parent = player.transform;
 			transform.gameObject.renderer.enabled = false;
 		}
 	}
-<<<<<<< HEAD
-
-	protected override void Update() {
-		base.Update ();
-	}
-=======
->>>>>>> origin/master
 }
