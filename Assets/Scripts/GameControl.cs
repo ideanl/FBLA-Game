67b5cacaa045f254ce16245d3ecffa3b9db4f428
Idean <idean1.labib@gmail.com>
2015-03-01 23:18:03 -0800

    Added menu

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class GameControl : MonoBehaviour {
 
 	public static GameControl control;
+
+	private GameObject hud;
+	private GameObject healthVal;
+	private GameObject level;
+	private GameObject menu;
+
+	public bool menuShown = false;
 
 	public float health;
 
@@ -12,12 +24,69 @@ public class GameControl : MonoBehaviour {
 		if (!control) {
 			DontDestroyOnLoad (gameObject);
 			control = this;
+
+			health = 100;
+			hud = gameObject.transform.Find ("Canvas/HUD").gameObject;
+			healthVal = hud.transform.Find ("HealthContainer/HealthVal").gameObject;
+			level = hud.transform.Find ("Level").gameObject;
+			menu = transform.Find ("PauseMenu").gameObject;
 		} else {
 			Destroy (gameObject);
 		}
 	}
 
-	void OnGUI() {
-		GUI.Label (new Rect (10, 10, 100, 30), "Health: " + health);
+	void Update() {
+		UpdateHUD ();
 	}
+
+	public void Save() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Open (Application.persistentDataPath + "/playerInfo.dat", FileMode.OpenOrCreate);
+
+		PlayerData data = new PlayerData ();
+		data.health = health;
+		data.level = Application.loadedLevel;
+		bf.Serialize (file, data);
+		file.Close ();
+	}
+
+	public void Load() {
+		String path = Application.persistentDataPath + "/playerInfo.dat";
+		if (File.Exists (path)) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (path, FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize (file);
+			file.Close();
+
+			health = data.health;
+			Application.LoadLevel (data.level);
+		}
+	}
+
+	public void ToggleMenu() {
+		menuShown = !menuShown;
+		foreach (UnityEngine.Object c in UnityEngine.Object.FindObjectsOfType (typeof(Canvas))) {
+			((Canvas) c).enabled = !menuShown;
+		}
+		menu.GetComponent<GUITexture>().enabled = menuShown;
+		menu.transform.Find ("Canvas").gameObject.GetComponent<Canvas>().enabled = menuShown;
+
+		if (Camera.main.GetComponent<MouseLook> ()) {
+			Camera.main.GetComponent<MouseLook> ().enabled = !menuShown;
+		}
+
+		//hud.SetActive (!menuShown);
+	}
+
+	void UpdateHUD() {
+		healthVal.GetComponent<Slider>().value = health / 100;
+		level.GetComponent<Text> ().text = "Level " + Application.loadedLevel;
+	}
+}
+
+[Serializable]
+class PlayerData
+{
+	public float health;
+	public int level;
 }
