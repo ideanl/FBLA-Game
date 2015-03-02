272ef751a39ebc4a7cc3cf68112a36fc54cbe326
Idean <idean1.labib@gmail.com>
2015-03-01 23:02:54 -0800

    fixed click

--- Assets/Scripts/Click/Click.cs
+++ Assets/Scripts/Click/Click.cs
@@ -4,18 +4,5 @@ using System.Collections;
 public abstract class Click : MonoBehaviour {
 
 	//Performs the action specific to the child class on click of the object.
-	protected abstract void ClickAction();
-
-	// Update is called once per frame
-	protected virtual void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-			if (Physics.Raycast (ray, out hit)) {
-				//Based on the name of the object, call ClickAction() in that class.
-				(hit.transform.GetComponent(hit.transform.name) as Click).ClickAction ();
-			}
-		}
-	}
+	public abstract void ClickAction();
 }
