cbe0d9ae4a2a11a70755bf4f4866c9ee18bc0a87
Andrew Hoang <andrewhoang03@gmail.com>
2015-03-09 23:05:10 -0700

    making jetpack work i hope

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class Jetpack : MonoBehaviour {
 	public static float FLOAT_SPEED = 0.3f;
-	public static float MAX_TIME = 3;
+	public static float MAX_TIME = 5;
 
 	private float currTime = 0;
 	private bool canFly = true;
 	private bool isUpgraded = false;
+	GameObject targetObj = null;
 
 	public void FlyJetpack(GameObject player) {
 		if (canFly) {
 			player.GetComponent<CharacterController> ().Move (player.transform.up * FLOAT_SPEED);
 		}
+	}
+
+	void Start() {
+		targetObj = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	void Update() {
@@ -30,6 +35,9 @@ public class Jetpack : MonoBehaviour {
 			canFly = false;
 		} else if (currTime > MAX_TIME * 2 && isUpgraded) {
 			canFly = false;
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2)) {
+			FlyJetpack(targetObj);
 		}
 	}
 
